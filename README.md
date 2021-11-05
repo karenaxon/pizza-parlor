e29515b97cb3712798bd970d2d71976d98bc8caa
Kaxon <karenaxon@users.noreply.github.com>
2021-11-05 10:16:08 -0700

    Add Pizza.prototype.cost = function() test.

--- README.md
+++ README.md
@@ -7,6 +7,13 @@
   let order1 = new Pizza;
   order1.size;
   Expected Output: undefined
+```
 
-
+**Pizza.prototype.cost = function()**
+```
+  Test: It should return the price of the pizza as $19
+  Code: 
+  let let order1 = new Pizza(2, [1, 4, 7, 8, 9]);
+  order1.cost();
+  Expected Output: $19
 ```
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,37 @@
-function Pizza (){
-  let toppings = [];
-  let size = "1";
+function Pizza (size, toppings){
+  this.toppings = toppings;
+  this.size = size;
 }
+
+
+Pizza.prototype.cost = function() {
+  let sum = 0;
+  if(this.size === 1) {
+    sum += 8;
+  } else if(this.size === 2) {
+    sum += 10;
+  } else {
+    sum += 12;
+  }
+
+  for(let i = 0; i < this.toppings.length; i++) {
+    if (this.toppings[i] === 1 || this.toppings[i] === 2 || this.toppings[i] === 3 || this.toppings[i] === 4) {
+      sum += 3;
+      console.log("+3");
+    } else if(this.toppings[i] === 11) {
+      sum += 2;
+      console.log("+2");
+    } else {
+      sum += 1;
+      console.log("+1");
+    }
+  } 
+  return sum;
+}
+
+
+let order1 = new Pizza(2, [1, 4, 7, 8, 9]);
+
+
+
 
